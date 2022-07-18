14855d626fefa9ca4e92b513493c9306b798af14
muzi288 <mzulu2204@gmail.com>
2022-07-18 16:12:24 +0200

    plotting code updated

--- src/plot.py
+++ src/plot.py
@@ -6,30 +6,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+
 plt.style.use('fivethirtyeight')
 
-#x_vals = []
-#y_vals = []
+x_values = []
+y_values = []
 
-#index = count()
+index = count()
 
 
 def animate(i):
-    data = pd.read_csv('data.csv')
-    y1 = data['thrust']
-    x = data['x_value']
-    #y2 = data['total_2']
-
+    x_values.append(next(index))
+    y_values.append(random.randint(0, 5))
     plt.cla()
-
-    plt.plot(x, y1, label='Channel 1')
-    #plt.plot(x, y2, label='Channel 2')
-
-    plt.legend(loc='upper left')
-    plt.tight_layout()
+    plt.plot(x_values, y_values)
 
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+ani = FuncAnimation(plt.gcf(), animate, 1000)
+
 
 plt.tight_layout()
 plt.show()
